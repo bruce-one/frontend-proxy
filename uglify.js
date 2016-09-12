c9d33e35fa4dd735fb04e4b29b2d751858f010f4
Bryce Gibson <bryce@gibson-consulting.com.au>
2016-09-13 09:09:53 +1000

    Uglifyjs settings.

--- uglify.js
+++ uglify.js
@@ -3,5 +3,24 @@
 const uglifyJs = require('uglify-js')
 
 process.on('message', (data) => {
-  process.send(uglifyJs.minify(data, { fromString: true }).code, () => process.exit(0))
+  process.send(uglifyJs.minify(data, {
+    fromString: true,
+    mangle: true,
+    compress: {
+      sequences: true,
+      dead_code: true,
+      conditionals: true,
+      booleans: true,
+      unused: true,
+      if_return: true,
+      join_vars: true,
+      drop_console: true,
+    },
+    output: {
+      beautify: false,
+      space_colon: false,
+      semicolons: false,
+      indent_level: 0,
+    }
+  }).code, () => process.exit(0))
 })
